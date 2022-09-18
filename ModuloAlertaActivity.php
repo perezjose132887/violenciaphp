e403538539607372faa47d52ba6f05622b604e44
perezjose132887 <perez.jose.132887@gmail.com>
2022-09-18 09:55:47 -0400

    Agregado modulo alerta version 1.0.0

---
+++ ModuloAlertaActivity.php
@@ -0,0 +1,21 @@
+<?php
+$conexion =mysqli_connect('localhost','root','perez','bdviolencia');
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+
+    $idUsuario=$_POST['idUsuario'];
+    $latitud=$_POST['latitud'];
+    $longitud=$_POST['longitud'];
+
+    $consulta="INSERT INTO alerta (idAlerta,idUsuario,latitud,longitud) VALUES(NULL,'$idUsuario','$latitud','$longitud')";
+
+    if(mysqli_query($conexion,$consulta)){
+        echo "Se subio exitosamente";
+    }else{
+        echo "error";
+    }
+
+}
+
+
+?>
